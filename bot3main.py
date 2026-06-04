983af5943b9877e42081eee2e0f62bb8599041f2
Olof123-hash <mathiasolofeinarljungberg@gmail.com>
2026-06-04 23:07:35 +0300

    Initial commit

---
+++ bot3main.py
@@ -0,0 +1,32 @@
+from imports import Imports
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+from commands import General
+from events import Events
+import asyncio
+
+##
+
+def configure ():
+    load_dotenv()
+
+channel_id = 1511678396504215562
+
+
+intents = discord.Intents.default()
+intents.members = True 
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+async def main():
+    configure()
+    await bot.add_cog(General(bot))
+    await bot.add_cog(Events(bot))
+    await bot.start(os.getenv('bot_token'))
+
+
+asyncio.run(main())
